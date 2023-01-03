ba9d18333d6a1ee5fc2e54b7718175d86ad7e933
Husain Fadhilah <husainazkas@gmail.com>
2023-01-03 10:07:51 +0700

    set platformVersion as deprecated api, this will removed soon

--- lib/flutter_zip_archive.dart
+++ lib/flutter_zip_archive.dart
@@ -5,6 +5,8 @@ import 'package:flutter/services.dart';
 class FlutterZipArchive {
   static const MethodChannel _channel = MethodChannel('flutter_zip_archive');
 
+  @Deprecated(
+      'This method will be removed soon due to method channel not being implemented on the platforms')
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
